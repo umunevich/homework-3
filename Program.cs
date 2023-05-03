c39cce135a09b93ba411d1660b6393dd1d6725c7
Yana <125117368+umunevich@users.noreply.github.com>
2023-05-03 18:49:21 +0300

    Задача з зірочкою

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum, digit;
+            Console.Write("Enter the number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            sum = 0;
+
+            while (num > 0)
+            {
+                digit = num%10;
+                sum += digit;
+                num = num/10;
+            }
+            Console.WriteLine("The summa of digits is {0}", sum);
+            Console.ReadKey(false);
+        }
+    }
+}
